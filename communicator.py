2297e515934234b5e0aa9cbd019df1f4418a8780
ysong-aceinna <ysong@aceinna.com>
2020-03-13 13:00:31 +0800

    rollback code of check available ports in function 'find_ports()'

--- communicator.py
+++ communicator.py
@@ -72,8 +72,17 @@ class SerialPort(Communicator):
                 A list of the serial ports available on the system
         '''
         print('scanning ports')
-        port_list = list(serial.tools.list_ports.comports())
-        ports = [ p.device for p in port_list]
+        # port_list = list(serial.tools.list_ports.comports())
+        # ports = [ p.device for p in port_list]
+        if sys.platform.startswith('win'):
+            ports = ['COM%s' % (i + 1) for i in range(256)]
+        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+            # this excludes your current terminal "/dev/tty"
+            ports = glob.glob('/dev/tty[A-Za-z]*')
+        elif sys.platform.startswith('darwin'):
+            ports = glob.glob('/dev/tty.*')
+        else:
+            raise EnvironmentError('Unsupported platform')
 
         result = []
         for port in ports:
